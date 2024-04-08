b5fc4cd448f733a0ade287eebf676c173ceb6574
«@Edu026» <edu.amoros26@gmail.com>
2024-04-08 17:19:34 +0200

    Game End in progress

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,31 +1,53 @@
 package com.badlogic.drop;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Drop extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
+
+public class Drop extends Game {
+
+	public SpriteBatch batch;
+	public BitmapFont font;
+
+	public boolean game_over = false;
+
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont(); // use libGDX's default Arial font
+		this.setScreen(new MainMenuScreen(this));
 	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render() {
+		super.render(); // important!
 	}
-	
-	@Override
-	public void dispose () {
+
+	public void pause(){
+		game_over = true;
+	}
+	public void resume(){
+		game_over = false;
+	}
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
+		this.setScreen(new EndMenuScreen(this));
 	}
+
 }
